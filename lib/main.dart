1b78ae5a9eeb6b99268c8e4df5aab0a1fe1d134b
rakseong <chltjdfkr12_@naver.com>
2022-02-24 17:57:05 +0900

    rr

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar icon menu'),
+        centerTitle: true,
+        elevation: 0.0,
+        actions: [
+          IconButton(
+              onPressed: () {
+                print('shopping cart is clicked');
+              },
+              icon: Icon(Icons.shopping_cart)),
+          IconButton(
+            onPressed: () {
+              print('search is clicked');
+            },
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/logo2.PNG'),
+                backgroundColor: Colors.white,
+              ),
+              accountName: Text('Raksoeng'),
+              accountEmail: Text('chltjdfkr12@naver.com'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
